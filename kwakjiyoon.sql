0e520195411d259bd487687ba29a93e7713c8501
zziyoon6227-g <zziyoon6227@gmail.com>
2026-04-22 17:22:19 +0900

    sql
    sql

---
+++ kwakjiyoon.sql
@@ -0,0 +1,23 @@
+select * from emp ; 
+select ename from emp ;
+select empno, ename from emp ; 
+select distinct job from emp; 
+select job from emp ;
+select job as b from emp ;
+select * from dept ;
+select * from dept where deptno = 30;
+select * from dept where deptno = 30 and dname = 'SALES';
+select * from emp where deptno = 30 or job = 'CLERK';
+select * from emp where sal >= 3000;
+select * from emp where ename >= 'F';
+select * from emp where sal != 3000;
+select * from emp where sal <> 3000;
+select * from emp where not sal = 3000;
+select * from emp where job in('MANAGER', 'SALESMAN', 'CLERK');
+select * from emp where job = 'MANAGER' or  job = 'SALESMAN' or  job = 'CLERK';
+select * from emp where sal between 2000 and 3000;
+select * from emp where ename like 'S%';
+select * from emp where ename like '%AM%';
+select * from emp where ename like '_L%';
+select * from emp where ename not like '%AM%';
+
