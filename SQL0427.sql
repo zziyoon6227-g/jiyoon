89e268e33726e2390047468036daf07ccc992683
zziyoon6227-g <zziyoon6227@gmail.com>
2026-04-27 17:41:14 +0900

    SQL0427
    4월 27일 오라클내용 다시 재업

--- SQL0427.sql
+++ SQL0427.sql
@@ -105,26 +105,70 @@ SELECT * FROM EMP_TRG;
 
 CREATE OR REPLACE TRIGGER trg_emp_nodml_weekend
 BEFORE
-INSERT OR UPDATE OR DELETE ON EMP_TRG
-BEGIN
-IF TO_CHAR(sysdate, 'DY') IN ('토','일') THEN
+INSERT OR UPDATE OR DELETE ON EMP_TRG --데이터 바꾸기 전에 트리거 실행됨
+BEGIN --트리거 시작
+IF TO_CHAR(sysdate, 'DY') IN ('월','일') THEN  --in은 or의미
 IF INSERTING THEN
 raise_application_error(-20000, '주말 사원정보 추가 불가');
+
 ELSIF UPDATING THEN
 raise_application_error(-20001, '주말 사원정보 수정 불가');
+
 ELSIF DELETING THEN
 raise_application_error(-20002, '주말 사원정보 삭제 불가');
+
 ELSE
 raise_application_error(-20003, '주말 사원정보 변경 불가');
 END IF;
 END IF;
 END;
 /
-UPDATE emp_trg SET sal = 3500 WHERE empno = 7788;
+UPDATE emp_trg SET sal = 3500 WHERE empno = 7788; 
+/*7788번은 sal 3500으로 변경해라 위부분 요일바꾸면 다시 컴파일거치고 업데이트
+실행하면 오류뜸*/
 
+CREATE TABLE EMP_TRG_LOG(
+TABLENAME VARCHAR2(10), -- DML이 수행된 테이블 이름
+DML_TYPE VARCHAR2(10),  -- DML 명령어의 종류
+EMPNO NUMBER(4), -- DML 대상이된사원번호
+USER_NAME VARCHAR2(30), -- DML을 수행한 USER 이름
+CHANGE_DATE DATE        -- DML이 수행된 날짜
+);
 
+CREATE OR REPLACE TRIGGER trg_emp_log
+AFTER
+INSERT OR UPDATE OR DELETE ON EMP_TRG
+FOR EACH ROW
+BEGIN
+IF INSERTING THEN
+INSERT INTO emp_trg_log
+VALUES ('EMP_TRG', 'INSERT', :new.empno,
+SYS_CONTEXT('USERENV', 'SESSION_USER'), sysdate);
+ELSIF UPDATING THEN
+INSERT INTO emp_trg_log
+VALUES ('EMP_TRG', 'UPDATE', :old.empno,
+SYS_CONTEXT('USERENV', 'SESSION_USER'), sysdate);
+ELSIF DELETING THEN
+INSERT INTO emp_trg_log
+VALUES ('EMP_TRG', 'DELETE', :old.empno,
+SYS_CONTEXT('USERENV', 'SESSION_USER'), sysdate);
+END IF;
+END;
 
-
+INSERT INTO EMP_TRG
+VALUES(9999, 'TestEmp', 'CLERK', 7788,
+TO_DATE('2018-03-03', 'YYYY-MM-DD'), 1200, null, 20);
+COMMIT;
+SELECT *
+FROM EMP_TRG;
+SELECT *
+FROM EMP_TRG_LOG;
+UPDATE EMP_TRG
+SET SAL = 1300
+WHERE MGR = 7788;
+COMMIT;
+SELECT TRIGGER_NAME, TRIGGER_TYPE, TRIGGERING_EVENT, TABLE_NAME, STATUS
+FROM USER_TRIGGERS;
 
 
 
